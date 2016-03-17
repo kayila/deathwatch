cc83f90b0774535a6f8c76833a22923b1e98e52c
Kayila <arekuanubis@gmail.com>
2016-03-17 19:06:33 -0400

    Ported to 1.8.9 and added killer name to the deathwatch file

--- java/com/kitsuneindustries/DeathWatch.java
+++ java/com/kitsuneindustries/DeathWatch.java
@@ -1,4 +1,4 @@
-package org.ponyelisium;
+package com.kitsuneindustries;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -7,24 +7,24 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.world.World;
 import net.minecraftforge.common.DimensionManager;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
-import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.Mod.EventHandler;
-import cpw.mods.fml.common.event.FMLServerStartingEvent;
-import cpw.mods.fml.common.eventhandler.EventPriority;
-import cpw.mods.fml.common.eventhandler.SubscribeEvent;
-import cpw.mods.fml.server.FMLServerHandler;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
+import net.minecraftforge.fml.common.eventhandler.EventPriority;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.registry.EntityRegistry;
 
 @Mod(modid = DeathWatch.MODID, name = DeathWatch.NAME, version = DeathWatch.VERSION, acceptableRemoteVersions = "*")
 public class DeathWatch {
 
     public static final String MODID = "deathwatch";
     public static final String NAME = "Death Watch";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "2.0";
     
     public static FileWriter file = null;
     public static File filepath = null;
@@ -41,20 +41,25 @@ public class DeathWatch {
     	return file;
     }
     
-    @EventHandler
+    @net.minecraftforge.fml.common.Mod.EventHandler
     public void serverLoad(FMLServerStartingEvent event) {
-    	MinecraftForge.EVENT_BUS.register(new org.ponyelisium.DeathWatch());
+    	MinecraftForge.EVENT_BUS.register(new com.kitsuneindustries.DeathWatch());
     }
     
     @SubscribeEvent(priority=EventPriority.NORMAL)
-    public void onLivingDeathEvent(LivingDeathEvent event) throws IOException {
+    public synchronized void onLivingDeathEvent(LivingDeathEvent event) throws IOException {
     	if(!event.entity.worldObj.isRemote) {
 	    	if(event.entity instanceof EntityPlayerMP) {
 	    		EntityPlayerMP player = (EntityPlayerMP)event.entity;
+	    		Entity killer = event.source.getEntity(); // If there is a killer
 	    		FileWriter output = this.getWriter();
-	    		output.write(sdf.format(Calendar.getInstance().getTime())+","+player.getDisplayName()+","+player.dimension+","+player.posX+","+player.posY+","+player.posZ+","+event.source.damageType+"\n");
+	    		
+	    		output.write(sdf.format(Calendar.getInstance().getTime())+","+player.getDisplayNameString()+","+player.dimension
+	    				+","+player.posX+","+player.posY+","+player.posZ+","+event.source.damageType
+	    				+(killer != null ? ","+killer.getName():"")
+	    				+"\n");
 	    		output.flush();
-	    		System.out.println(sdf.format(Calendar.getInstance().getTime())+","+player.getDisplayName()+","+player.dimension+","+player.posX+","+player.posY+","+player.posZ+","+event.source.damageType);
+	    		System.out.println(sdf.format(Calendar.getInstance().getTime())+","+player.getDisplayNameString()+","+player.dimension+","+player.posX+","+player.posY+","+player.posZ+","+event.source.damageType);
 	    	}
     	}
     }
